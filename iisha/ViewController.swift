c29a375e519d8a6e1e7eca4a36c93204063b9c52
張翔 <chosho.0101@softbank.ne.jp>
2018-12-28 14:37:55 +0900

    Initial Commit

---
+++ iisha/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iisha
+//
+//  Created by 張翔 on 2018/12/28.
+//  Copyright © 2018 張翔. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
