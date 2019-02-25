5ecf172eb959af15d947c0018e4e16865ac49d0f
touyou <fujiyou1121@gmail.com>
2019-02-26 01:48:00 +0900

    change file structure and create new project\

---
+++ JapanJourneyQuest/JJQ/JJQ/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JJQ
+//
+//  Created by 藤井陽介 on 2019/02/26.
+//  Copyright © 2019 Swimee Winter Camp 2019 Manager. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
