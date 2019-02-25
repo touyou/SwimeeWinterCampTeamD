c78268a9630819a47e9641dedbf61c2cc59a7844
touyou <fujiyou1121@gmail.com>
2019-02-26 01:59:36 +0900

    ストーリーボード分割

---
+++ JapanJourneyQuest/JJQ/JJQ/Scenes/DetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailViewController.swift
+//  JJQ
+//
+//  Created by 藤井陽介 on 2019/02/26.
+//  Copyright © 2019 Swimee Winter Camp 2019 Manager. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ JapanJourneyQuest/JJQ/JJQ/Scenes/ItemListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ItemListViewController.swift
+//  
+//
+//  Created by 藤井陽介 on 2019/02/26.
+//
+
+import UIKit
+
+class ItemListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
